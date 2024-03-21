5b084f066aa6fc07242ec78147c79c1949092ec0
Andrey-0367 <shestakov0367@yandex.ru>
2024-03-21 16:15:58 +0300

    proverka

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Проектная работа Mesto
-
+[https://github.com/Andrey-0367/mesto-project-ff.git]
+### Автор проекта:
+Андрей Шестаков
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,22 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const cardsList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+
+initialCards.forEach(function (element) {
+    const cardElement = cardTemplate.cloneNode(true);
+
+    cardElement.querySelector('.card__title').textContent = element.name;
+    cardElement.querySelector('.card__image').src = element.link;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+        let item = evt.target.closest('.places__item');
+        item.remove()
+    });
+
+    cardsList.append(cardElement)
+})
